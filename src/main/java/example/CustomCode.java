04334cf91378a9993d06b7a96d7ca0b816cf62d5
Antoine Vinot <antoine.vinot@sonarsource.com>
2022-08-31 18:10:48 +0200

    Test

--- src/main/java/example/CustomCode.java
+++ src/main/java/example/CustomCode.java
@@ -38,4 +38,12 @@ public class CustomCode {
       throw new RuntimeException(e);
     }
   }
+
+  public void myVulnerability4() {
+    try {
+      DriverManager.getConnection("jdbc:derby:memory:myDB;create=true", "login", "");
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
 }
